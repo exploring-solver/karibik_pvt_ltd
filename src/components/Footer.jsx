c57877e807e8aaed8bf97a6b4de65d370f74eef6
exploring-solver <amansharma12607@gmail.com>
2024-03-23 22:35:05 +0530

    ya

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,52 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='bg-gray-800 opacity-5 '>
-        <div id='reach'></div>
-        <div id='quick-links'></div>
-        <div id='campus'></div>
-        <div id='contact'></div>
-        <div id='copyright-text'></div>
+    <div className='bg-black bg-opacity-95 text-gray-400 py-4 '>
+      <div className='flex justify-between w-[80%] mx-auto'>
+        <div id='reach'>
+          <h1 className='font-semibold italic'>HOW TO REACH US</h1>
+          <hr className='py-[0.5px] w-[40%] bg-yellow-500' />
+
+        </div>
+        <div id='quick-links' className='flex flex-col'>
+          <h1 className='font-semibold italic'>QUICK LINKS</h1>
+          <hr className='py-[0.5px] w-[40%] bg-yellow-500' />
+          <p>Courses</p>
+          <p>Courses</p>
+          <p>Courses</p>
+          <p>Courses</p>
+          <p>Courses</p>
+        </div>
+        <div id='campus' className='flex flex-col'>
+          <h1 className='font-semibold italic'>QUICK LINKS</h1>
+          <hr className='py-[0.5px] w-[40%] bg-yellow-500' />
+          <p>Courses</p>
+          <p>Courses</p>
+          <p>Courses</p>
+          <p>Courses</p>
+          <p>Courses</p>
+        </div>
+        <div id='contact' className='flex flex-col'>
+          <h1 className='font-semibold italic'>QUICK LINKS</h1>
+          <hr className='py-[0.5px] w-[40%] bg-yellow-500' />
+          <p>Courses</p>
+          <p>Courses</p>
+          <p>Courses</p>
+          <p>Courses</p>
+          <p>Courses</p>
+        </div>
+      </div>
+      <br />
+      <div id='copyright-text ' className='flex justify-between w-[80%] mx-auto'>
+        <div>
+          <h1>Copyright © Merchant Navy Institute & Training Center 2021. All rights reserved.</h1>
+        </div>
+        <div className='flex justify-between gap-2'>
+          <p className='px-2 border-r-2'>Privacy Policy</p>
+          <p className='px-2 border-r-2'>Privacy Policy</p>
+          <p className='px-2 border-r-2'>Privacy Policy</p>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -18,7 +18,7 @@ function Home() {
   };
 
   return (
-    <div className="container mx-auto mt-5 bg-zinc-200">
+    <div className=" bg-zinc-200">
       {/* <h1 className="text-3xl font-bold mb-5">Welcome to CatalogCraft</h1> */}
       <Slider {...settings}>
         <div className=''>
@@ -45,25 +45,157 @@ function Home() {
           />
         </div>
       </Slider>
-      <div id='about' className=''>
-        <div className='about-img'>
+      <br /><br />
+      <div id='about' className='flex w-[80%] mx-auto flex-wrap'>
+        <div className='about-img mr-[10%]'>
+          <img src="SAILOR.jpg" alt="" />
+        </div>
+        <div className='about-content w-[70%]'>
+          <h1 className='text-2xl font-semibold border-b-2 py-3'>ABOUT MERCHANT NAVY INSTITUTE & TRAINING CENTER</h1>
+          <hr className='py-[2px] w-[20%] bg-yellow-500' />
+          <br /><p className='text-sm leading-[1.75]'>
+            <span className='font-semibold text-base'>Its a Unit of Navigation Maritime Services</span>
+            <br />
+            MNITC is an India’s leading Maritime Education provider located in Jaipur Rajasthan and is gradually emerging as a leader. we are working successfully last 13 years. MNITC has created the market of trust and not illusions. we assure you the best placement at highest packages accompanied by raising graph. we have an active placement team.
+          </p>
+        </div>
+      </div>
+
+      <div id='courses' className=' w-[80%] mx-auto'>
+        <div>
+          <h1 className='text-2xl font-semibold border-b-2 py-3'>OUR COURSES</h1>
+          <hr className='py-[2px] w-[5%] bg-yellow-500' />
+        </div>
+        <br />
+        <div className='flex flex-col md:flex-row md:flex-wrap gap-2'>
+          <div className="courses flex lg:w-[45%]">
+            <div className='mr-[10%]'><img src="merchant-about.jpeg" className='' alt="" /></div>
+            <div className='w-[70%]'>
+              <h1 className='font-semibold text-lg'>SDP</h1>
+              <p className='text-gray-700 text-sm w-[200px]'>The PRE SEA TRAINING Course was introduced on request from the shopping industry.</p>
+              <br />
+              <button className='bg-yellow-500 p-2 font-semibold'>READ MORE </button>
+            </div>
+          </div>
+          <div className="courses flex lg:w-[45%]">
+            <div className='mr-[10%]'><img src="merchant-about.jpeg" className='' alt="" /></div>
+            <div className='w-[70%]'>
+              <h1 className='font-semibold text-lg'>SDP</h1>
+              <p className='text-gray-700 text-sm w-[200px]'>The PRE SEA TRAINING Course was introduced on request from the shopping industry.</p>
+              <br />
+              <button className='bg-yellow-500 p-2 font-semibold'>READ MORE </button>
+            </div>
+          </div>
+          <div className="courses flex lg:w-[45%]">
+            <div className='mr-[10%]'><img src="merchant-about.jpeg" className='' alt="" /></div>
+            <div className='w-[70%]'>
+              <h1 className='font-semibold text-lg'>SDP</h1>
+              <p className='text-gray-700 text-sm w-[200px]'>The PRE SEA TRAINING Course was introduced on request from the shopping industry.</p>
+              <br />
+              <button className='bg-yellow-500 p-2 font-semibold'>READ MORE </button>
+            </div>
+          </div>
+          <div className="courses flex lg:w-[45%]">
+            <div className='mr-[10%]'><img src="merchant-about.jpeg" className='' alt="" /></div>
+            <div className='w-[70%]'>
+              <h1 className='font-semibold text-lg'>SDP</h1>
+              <p className='text-gray-700 text-sm w-[200px]'>The PRE SEA TRAINING Course was introduced on request from the shopping industry.</p>
+              <br />
+              <button className='bg-yellow-500 p-2 font-semibold'>READ MORE </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='bg-[#183650] py-5 my-10'>
+        <div id='apply' className=' w-[80%] mx-auto flex items-center justify-center'>
+          <div className='mr-[15%] flex justify-center items-center'>
+            <img src="full-man.jpg" className='' alt="" />
+          </div>
+          <div className='flex flex-wrap w-[85%]  text-white '>
+            <div className='w-[300px]'>
+              <div className='flex items-center gap-2 '>
+                <i class="fa fa-book" aria-hidden="true"></i>
+                <div className=''>
+                  <h1 className='font-semibold'>APPLY ONLINE</h1>
+                  <hr className='py-[1px] w-[20%] bg-yellow-700' />
+                </div>
+              </div>
+              <p className='text-sm w-[200px] text-gray-400'>Available courses are gp rating, gme, dns, btech, eto, etc.</p>
+
+            </div>
+            <div className='w-[300px]'>
+              <div className='flex items-center gap-2 '>
+                <i class="fa fa-book" aria-hidden="true"></i>
+                <div className=''>
+                  <h1 className='font-semibold'>APPLY ONLINE</h1>
+                  <hr className='py-[1px] w-[20%] bg-yellow-700' />
+                </div>
+              </div>
+              <p className='text-sm w-[200px] text-gray-400'>Available courses are gp rating, gme, dns, btech, eto, etc.</p>
+
+            </div>
+            <div className='w-[300px]'>
+              <div className='flex items-center gap-2 '>
+                <i class="fa fa-book" aria-hidden="true"></i>
+                <div className=''>
+                  <h1 className='font-semibold'>APPLY ONLINE</h1>
+                  <hr className='py-[1px] w-[20%] bg-yellow-700' />
+                </div>
+              </div>
+              <p className='text-sm w-[200px] text-gray-400'>Available courses are gp rating, gme, dns, btech, eto, etc.</p>
+
+            </div>
+            <div className='w-[300px]'>
+              <div className='flex items-center gap-2 '>
+                <i class="fa fa-book" aria-hidden="true"></i>
+                <div className=''>
+                  <h1 className='font-semibold'>APPLY ONLINE</h1>
+                  <hr className='py-[1px] w-[20%] bg-yellow-700' />
+                </div>
+              </div>
+              <p className='text-sm w-[200px] text-gray-400'>Available courses are gp rating, gme, dns, btech, eto, etc.</p>
+
+            </div>
+          </div>
 
         </div>
-        <div className='about-content'>
-
+      </div>
+      <div id='services' className='w-[80%] mx-auto pb-[5%]'>
+        <div>
+          <h1 className='text-2xl font-semibold border-b-2 py-3'>OUR FEATURED SERVICES</h1>
+          <hr className='py-[2px] w-[5%] bg-yellow-500' />
         </div>
-      </div>
-      <div id='courses' className=''>
-        <div className="courses"></div>
-        <div className="courses"></div>
-        <div className="courses"></div>
-        <div className="courses"></div>
-      </div>
-      <div id='apply' className=''>
-        
-      </div>
-      <div id='services' className=''>
-
+        <br />
+        <div className='flex flex-wrap gap-2'>
+          <div className='bg-white hover:bg-[#00E6F6] hover:cursor-pointer flex lg:w-[45%] justify-between'>
+            <img src="SAILOR.jpg" alt="" className='w-40 mr-[10%]' />
+            <div className='flex flex-col justify-center pr-10 '>
+              <h1 className=' font-semibold'>TYPES OF SHIPS</h1>
+              <p className='text-xs w-[80%]'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates, quas doloremque dolor culpa quia natus est voluptate asperiores omnis inventore.</p>
+            </div>
+          </div>
+          <div className='bg-white hover:bg-[#00E6F6] hover:cursor-pointer flex lg:w-[45%] justify-between'>
+            <img src="SAILOR.jpg" alt="" className='w-40 mr-[10%]' />
+            <div className='flex flex-col justify-center pr-10 '>
+              <h1 className=' font-semibold'>TYPES OF SHIPS</h1>
+              <p className='text-xs w-[80%]'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates, quas doloremque dolor culpa quia natus est voluptate asperiores omnis inventore.</p>
+            </div>
+          </div>
+          <div className='bg-white hover:bg-[#00E6F6] hover:cursor-pointer flex lg:w-[45%] justify-between'>
+            <img src="SAILOR.jpg" alt="" className='w-40 mr-[10%]' />
+            <div className='flex flex-col justify-center pr-10 '>
+              <h1 className=' font-semibold'>TYPES OF SHIPS</h1>
+              <p className='text-xs w-[80%]'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates, quas doloremque dolor culpa quia natus est voluptate asperiores omnis inventore.</p>
+            </div>
+          </div>
+          <div className='bg-white hover:bg-[#00E6F6] hover:cursor-pointer flex lg:w-[45%] justify-between'>
+            <img src="SAILOR.jpg" alt="" className='w-40 mr-[10%]' />
+            <div className='flex flex-col justify-center pr-10 '>
+              <h1 className=' font-semibold'>TYPES OF SHIPS</h1>
+              <p className='text-xs w-[80%]'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates, quas doloremque dolor culpa quia natus est voluptate asperiores omnis inventore.</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
